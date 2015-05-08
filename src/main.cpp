bd4953c11ac6ceb9eb0e25554e79ba91bf7834f1
Jethro Muller <mlljet001@myuct.ac.za>
2015-05-08 11:20:25 +0200

    My idea to typedef the int types doesn't work. Now I need all the if statements.

--- src/main.cpp
+++ src/main.cpp
@@ -18,11 +18,6 @@ int main(int argc,  const char* argv[]) {
             return 0;
         }
 
-        typedef int8_t sampleRateType;
-        if (bitCount == 16) {
-            typedef int16_t sampleRateType;
-        }
-
         int numChannels = stoi(argv[6]);
         if (numChannels != 1 && numChannels != 2) {
             cout << "Unsupported channel count." << endl;
@@ -43,18 +38,43 @@ int main(int argc,  const char* argv[]) {
             infile = argv[8 + mod];
             string infile2 = argv[9 + mod];
             if (numChannels == 1) {
-                MLLJET001::Audio<sampleRateType, 1> audio1(infile, sampleRate);
-                MLLJET001::Audio<sampleRateType, 1> audio2(infile2, sampleRate);
-                (audio1 + audio2).saveFile(outfileName);
+                if (bitCount == 16) {
+                    MLLJET001::Audio<int16_t, 1> audio1(infile, sampleRate);
+                    MLLJET001::Audio<int16_t, 1> audio2(infile2, sampleRate);
+                    (audio1 + audio2).saveFile(outfileName);
+                } else {
+                    MLLJET001::Audio<int8_t, 1> audio1(infile, sampleRate);
+                    MLLJET001::Audio<int8_t, 1> audio2(infile2, sampleRate);
+                    (audio1 + audio2).saveFile(outfileName);
+                }
             } else {
-                MLLJET001::Audio<sampleRateType, 2> audio1(infile, sampleRate);
-                MLLJET001::Audio<sampleRateType, 2> audio2(infile2, sampleRate);
-                (audio1 + audio2).saveFile(outfileName);
+                if (bitCount == 16) {
+                    MLLJET001::Audio<int16_t, 2> audio1(infile, sampleRate);
+                    MLLJET001::Audio<int16_t, 2> audio2(infile2, sampleRate);
+                    (audio1 + audio2).saveFile(outfileName);
+                } else {
+                    MLLJET001::Audio<int8_t, 2> audio1(infile, sampleRate);
+                    MLLJET001::Audio<int8_t, 2> audio2(infile2, sampleRate);
+                    (audio1 + audio2).saveFile(outfileName);
+                }
             }
         } else if (op == "-cut") {
             int r1 = stoi(argv[8 + mod]);
             int r2 = stoi(argv[9 + mod]);
             infile = argv[10 + mod];
+            if (numChannels == 1) {
+                if (bitCount == 16) {
+                    MLLJET001::Audio<int16_t, 1> audio(infile, sampleRate);
+                } else {
+                    MLLJET001::Audio<int8_t, 1> audio(infile, sampleRate);
+                }
+            } else {
+                if (bitCount == 16) {
+                    MLLJET001::Audio<int16_t, 2> audio(infile, sampleRate);
+                } else {
+                    MLLJET001::Audio<int8_t, 2> audio(infile, sampleRate);
+                }
+            }
         } else if (op == "-radd") {
             int r1 = stoi(argv[8 + mod]);
             int r2 = stoi(argv[9 + mod]);
@@ -62,53 +82,147 @@ int main(int argc,  const char* argv[]) {
             int s2 = stoi(argv[11 + mod]);
             infile = argv[12 + mod];
             string infile2 = argv[13 + mod];
+            if (numChannels == 1) {
+                if (bitCount == 16) {
+                    MLLJET001::Audio<int16_t, 1> audio(infile, sampleRate);
+                } else {
+                    MLLJET001::Audio<int8_t, 1> audio(infile, sampleRate);
+                }
+            } else {
+                if (bitCount == 16) {
+                    MLLJET001::Audio<int16_t, 2> audio(infile, sampleRate);
+                } else {
+                    MLLJET001::Audio<int8_t, 2> audio(infile, sampleRate);
+                }
+            }
         } else if (op == "-cat") {
             infile = argv[8 + mod];
             string infile2 = argv[9 + mod];
             if (numChannels == 1) {
-                MLLJET001::Audio<sampleRateType, 1> audio1(infile, sampleRate);
-                MLLJET001::Audio<sampleRateType, 1> audio2(infile2, sampleRate);
-                (audio1 | audio2).saveFile(outfileName);
+                if (bitCount == 16) {
+                    MLLJET001::Audio<int16_t, 1> audio1(infile, sampleRate);
+                    MLLJET001::Audio<int16_t, 1> audio2(infile2, sampleRate);
+                    (audio1 | audio2).saveFile(outfileName);
+                } else {
+                    MLLJET001::Audio<int8_t, 1> audio1(infile, sampleRate);
+                    MLLJET001::Audio<int8_t, 1> audio2(infile2, sampleRate);
+                    (audio1 | audio2).saveFile(outfileName);
+                }
             } else {
-                MLLJET001::Audio<sampleRateType, 2> audio1(infile, sampleRate);
-                MLLJET001::Audio<sampleRateType, 2> audio2(infile2, sampleRate);
-                (audio1 | audio2).saveFile(outfileName);
+                if (bitCount == 16) {
+                    MLLJET001::Audio<int16_t, 2> audio1(infile, sampleRate);
+                    MLLJET001::Audio<int16_t, 2> audio2(infile2, sampleRate);
+                    (audio1 | audio2).saveFile(outfileName);
+                } else {
+                    MLLJET001::Audio<int8_t, 2> audio1(infile, sampleRate);
+                    MLLJET001::Audio<int8_t, 2> audio2(infile2, sampleRate);
+                    (audio1 | audio2).saveFile(outfileName);
+                }
             }
         } else if (op == "-v") {
             float r1 = stof(argv[8 + mod]);
             float r2 = stof(argv[9 + mod]);
             infile = argv[10 + mod];
             if (numChannels == 1) {
-                MLLJET001::Audio<sampleRateType, 1> audio1(infile, sampleRate);
-                (audio1 * std::make_pair(r1, r2));
-                audio1.saveFile(outfileName);
+                if (bitCount == 16) {
+                    MLLJET001::Audio<int16_t, 1> audio1(infile, sampleRate);
+                    (audio1 * std::make_pair(r1, r2));
+                    audio1.saveFile(outfileName);
+                } else {
+                    MLLJET001::Audio<int8_t, 1> audio1(infile, sampleRate);
+                    (audio1 * std::make_pair(r1, r2));
+                    audio1.saveFile(outfileName);
+                }
             } else {
-                MLLJET001::Audio<sampleRateType, 2> audio1(infile, sampleRate);
-                (audio1 * std::make_pair(r1, r2));
-                audio1.saveFile(outfileName);
+                if (bitCount == 16) {
+                    MLLJET001::Audio<int16_t, 2> audio1(infile, sampleRate);
+                    (audio1 * std::make_pair(r1, r2));
+                    audio1.saveFile(outfileName);
+                } else {
+                    MLLJET001::Audio<int8_t, 2> audio1(infile, sampleRate);
+                    (audio1 * std::make_pair(r1, r2));
+                    audio1.saveFile(outfileName);
+                }
             }
         } else if (op == "-rev") {
             infile = argv[8 + mod];
+            if (numChannels == 1) {
+                if (bitCount == 16) {
+                    MLLJET001::Audio<int16_t, 1> audio(infile, sampleRate);
+                } else {
+                    MLLJET001::Audio<int8_t, 1> audio(infile, sampleRate);
+                }
+            } else {
+                if (bitCount == 16) {
+                    MLLJET001::Audio<int16_t, 2> audio(infile, sampleRate);
+                } else {
+                    MLLJET001::Audio<int8_t, 2> audio(infile, sampleRate);
+                }
+            }
         } else if (op == "-rms") {
             infile = argv[8 + mod];
             if (numChannels == 1) {
-                MLLJET001::Audio<sampleRateType, 1> audio1(infile, sampleRate);
-                cout << audio1.calculateRMS() << endl;
+                if (bitCount == 16) {
+                    MLLJET001::Audio<int16_t, 1> audio1(infile, sampleRate);
+                } else {
+                    MLLJET001::Audio<int8_t, 1> audio1(infile, sampleRate);
+                }
             } else {
-                MLLJET001::Audio<sampleRateType, 2> audio1(infile, sampleRate);
-                std::pair<float, float> rms = audio1.calculateRMS();
-                cout << rms.first << " " << rms.second << endl;
+                if (bitCount == 16) {
+                    MLLJET001::Audio<int16_t, 2> audio1(infile, sampleRate);
+                } else {
+                    MLLJET001::Audio<int8_t, 2> audio1(infile, sampleRate);
+                }
             }
         } else if (op == "-norm") {
             int r1 = stoi(argv[8 + mod]);
             int r2 = stoi(argv[9 + mod]);
             infile = argv[10 + mod];
+            if (numChannels == 1) {
+                if (bitCount == 16) {
+                    MLLJET001::Audio<int16_t, 1> audio(infile, sampleRate);
+                } else {
+                    MLLJET001::Audio<int8_t, 1> audio(infile, sampleRate);
+                }
+            } else {
+                if (bitCount == 16) {
+                    MLLJET001::Audio<int16_t, 2> audio(infile, sampleRate);
+                } else {
+                    MLLJET001::Audio<int8_t, 2> audio(infile, sampleRate);
+                }
+            }
         } else if (op == "-fadein") {
             float s = stof(argv[8 + mod]);
             infile = argv[9 + mod];
+            if (numChannels == 1) {
+                if (bitCount == 16) {
+                    MLLJET001::Audio<int16_t, 1> audio(infile, sampleRate);
+                } else {
+                    MLLJET001::Audio<int8_t, 1> audio(infile, sampleRate);
+                }
+            } else {
+                if (bitCount == 16) {
+                    MLLJET001::Audio<int16_t, 2> audio(infile, sampleRate);
+                } else {
+                    MLLJET001::Audio<int8_t, 2> audio(infile, sampleRate);
+                }
+            }
         } else if (op == "-fadeout") {
             float s = stof(argv[8 + mod]);
             infile = argv[9 + mod];
+            if (numChannels == 1) {
+                if (bitCount == 16) {
+                    MLLJET001::Audio<int16_t, 1> audio(infile, sampleRate);
+                } else {
+                    MLLJET001::Audio<int8_t, 1> audio(infile, sampleRate);
+                }
+            } else {
+                if (bitCount == 16) {
+                    MLLJET001::Audio<int16_t, 2> audio(infile, sampleRate);
+                } else {
+                    MLLJET001::Audio<int8_t, 2> audio(infile, sampleRate);
+                }
+            }
         }
     } catch (exception& e) {
         cout << "Malformed argument list." << endl;
